791c7c7689ffc1b410b911c1da636b02ee8451a3
yi lu <luyikk@126.com>
2021-01-18 15:13:07 +0800

    init

---
+++ test_server/src/main.rs
@@ -0,0 +1,19 @@
+mod test_controller;
+mod client;
+
+use std::error::Error;
+use netxserver::{NetXServer, ServerOption};
+use netxserver::log::*;
+use crate::test_controller::ImplCreateController;
+
+
+#[tokio::main]
+async fn main()->Result<(),Box<dyn Error>> {
+    env_logger::Builder::default().filter_level(LevelFilter::Debug).init();
+    let server=
+        NetXServer::new(ServerOption::new("0.0.0.0:6666","","123123"),
+                        ImplCreateController).await;
+    server.start_block().await?;
+
+    Ok(())
+}
