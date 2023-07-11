1a0431b0e6667078422c5356d7c17b819304f5f7
luyi <luyikk@126.com>
2023-07-11 12:16:50 +0800

    style(client): fmt

--- netx_client/src/client/impl_client.rs
+++ netx_client/src/client/impl_client.rs
@@ -243,7 +243,7 @@ impl<T: SessionSave + 'static> NetXClient<T> {
             match cmd {
                 1000 => match dr.read_fixed::<bool>()? {
                     false => {
-                        let msg=dr.read_fixed_str()?;
+                        let msg = dr.read_fixed_str()?;
                         log::debug!("{server_info} {msg}");
                         if (dr.len() - dr.get_offset()) == 1 && dr.read_fixed::<u8>()? == 1 {
                             log::debug!("mode 1");
